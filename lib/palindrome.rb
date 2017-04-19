fa45fb59cf54cf10cd346fbcc6e39ade12d96d27
Chitroopa Manikkavasagam and Dan Jacob <dan@>
2017-04-19 11:31:57 -0700

    Add palindrome function without reverse method

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -8,8 +8,26 @@ class String
      false
     end
   end
+
+ # without using reverse method 
+  define_method(:is_palindrome_without_reverse?) do
+    input_string = self.downcase().delete(' ')
+    split_input_string = input_string.split("")
+    reverse_array = []
+    while split_input_string.length > 0 do
+      letter = split_input_string.pop()
+      reverse_array.push(letter)
+    end
+    reverse_string = reverse_array.join()
+    if reverse_string == input_string
+      true
+    else
+     false
+    end
+  end
+
   define_method(:palindrome) do
-    result = self.is_palindrome?()
+    result = self.is_palindrome_without_reverse?()
     result_statement = ""
     if result
       result_statement = "You have a palindrome!"
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -14,6 +14,12 @@ describe('String#palindrome') do
   it ('returns true if sentence is palindrome') do
     expect((" Nurses run ").is_palindrome?()).to(eq(true))
   end
+  it ('returns false if string is not a palindrome') do
+    expect(("not").is_palindrome_without_reverse?()).to(eq(false))
+  end
+  it ('returns true if string is palindrome') do
+    expect(("madam").is_palindrome_without_reverse?()).to(eq(true))
+  end
   it ('returns sentence if your string is a palindrome or not') do
     expect((" Nurses run ").palindrome()).to(eq("You have a palindrome!"))
   end
